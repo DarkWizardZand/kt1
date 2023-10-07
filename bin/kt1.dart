9e3906c8a09ce95157e558ff387d6d192dbbcb04
DarkWizardZand <wizard102004@yandex.ru>
2023-10-07 09:59:23 +0500

    end

--- bin/kt1.dart
+++ bin/kt1.dart
@@ -5,8 +5,12 @@ void main(List<String> arguments) async {
   Dio client = Dio();
   String url = "https://dummyjson.com/products";
   Response res = await client.get(url);
+  Map<String, dynamic> data = res.data;
   int count = 0;
-  for (var element in res.data) {
-    
+  for (var element in data['products']) {
+    if(element['brand'] == 'Apple'){
+      count++;
+    }
   }
+  print(count);
 }
